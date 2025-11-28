9b81231385a44597af5d1ac6cc7725f5365c8b76
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-11-29 01:14:40 +0530

    Learnt models, schema creation and routing

---
+++ backend/models/Notes.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const NotesSchema = new mongoose.Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  tag:{
+    type:String,
+   default:"General" 
+  },
+  time:{
+    type:Date,
+    default:Date.now 
+  }
+  
+});
+
+module.exports=mongoose.model('user',UserSchema);
