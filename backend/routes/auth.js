071668443f7b7d18b9c1e1c53fe86636129982b5
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-12-16 13:37:27 +0530

    adding data validation using express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,31 @@
 const express=require('express');
 const User=require("../models/User");
+const {body,validationResult } = require('express-validator');
 const router=express.Router();
 
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
+router.post('/',[
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:5}),
+], async (req,res)=>{
+     const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+try {
+      // Create user
+      const user = new User(req.body);
+      await user.save();
 
-    res.send(req.body)
+      // Send response
+      res.status(201).json({
+        success: true,
+        message: "User created successfully"
+      });
+
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
 })
 
 module.exports=router;
